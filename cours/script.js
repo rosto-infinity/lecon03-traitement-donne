f7e37c39008ab23c4de2c2cd878216a1dd4b3aef
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-27 16:01:55 +0100

    Stocké les données

--- cours/script.js
+++ cours/script.js
@@ -1,19 +1,19 @@
-let string ="Hello World";
-console.log(typeof(string)) // string;
+let string = "Hello World";
+console.log(typeof (string)) // string;
 
 let number = 42;
-console.log(typeof(number)) // number;
- 
+console.log(typeof (number)) // number;
+
 let boolean = true;
-console.log(typeof(boolean)) // boolean;
+console.log(typeof (boolean)) // boolean;
 
 
-let object_null =null
+let object_null = null
 
 console.log(object_null);
 
-let array = [1,2,3,4,5];
-console.log(typeof(array)) // object;
+let array = [1, 2, 3, 4, 5];
+console.log(typeof (array)) // object;
 
 let nbre;
 console.log(nbre) // undefined;
@@ -22,32 +22,32 @@ console.log(nbre) // undefined;
 // Les tableaux
 
 // ---tableaux- 01
-let array2 = ["Bafou","Batie","Bafang","Bamenda"];
+let array2 = ["Bafou", "Batie", "Bafang", "Bamenda"];
 
 console.log(array2[0][2]); // f
 
-console.log(array2.length ); // 4
+console.log(array2.length); // 4
 
 // ---Object
-let person= {
-    name: "Bafou",
-    age: 42,
-    isTrue: true,
-    city: "Bafang",
-    country: "Cameroon",
-    isNull: null
+let person = {
+  name: "Bafou",
+  age: 42,
+  isTrue: true,
+  city: "Bafang",
+  country: "Cameroon",
+  isNull: null
 };
 
-console.log(person.name); 
-console.log(person.country[1]); 
+console.log(person.name);
+console.log(person.country[1]);
 
-let nom ='Myra'
-console.log(nom.length); 
+let nom = 'Myra'
+console.log(nom.length);
 
 // ----tableaux- 02
-let array3 = ["Bafou",47,true ,"Bafang","Bamenda",null,{
-    name: "Myra",
-    age: 42,
+let array3 = ["Bafou", 47, true, "Bafang", "Bamenda", null, {
+  name: "Myra",
+  age: 42,
 }];
 
 // affiche 42 ==
@@ -63,4 +63,39 @@ let objet = {
   age: 29,
   technos: ["Javascript", "NodeJs", "React", "laravel", "Flutter"],
   admin: false
-  }
+}
+
+console.log(typeof (objet)); // object
+
+// affiche la techno react
+console.log(objet.technos[2]);
+
+objet.adress = "Bafoussam";
+console.log(objet);
+
+
+// Stocké les données
+let data = [
+  {
+    firstname: "rosto1",
+    lastname: "dev1",
+    age: 30,
+    technos: ["Javascript", "NodeJs", "React", "laravel", "Flutter"],
+    adress: "MarchéB",
+    admin: false
+  },
+  {
+    firstname: "rosto2",
+    lastname: "dev2",
+    age: 29,
+    technos: ["Php", "NodeJs", "React", "Python", "Flutter"],
+    admin: false
+  },
+  {
+    firstname: "rosto3",
+    lastname: "dev3",
+    age: 32,
+    technos: ["CSS", "NodeJs", "React", "Taillwindcss", "Flutter"],
+    admin: true
+  },
+];
