65382f5b6f394606b46c9a62242093eee39c9c67
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-27 15:08:43 +0100

    types de données

---
+++ cours/script.js
@@ -0,0 +1,16 @@
+let string ="Hello World";
+console.log(typeof(string)) // string;
+
+let number = 42;
+console.log(typeof(number)) // number;
+ 
+let boolean = true;
+console.log(typeof(boolean)) // boolean;
+
+
+let object_null =null
+
+console.log(object_null);
+
+let array = [1,2,3,4,5];
+console.log(typeof(array)) // object;
